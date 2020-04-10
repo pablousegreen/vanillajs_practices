2665c1d2bb7ba369b34109542fbb622f600ce2db
Pablo Gonzalez Ramirez <pablo.gonzalez@openpay.mx>
2020-04-10 17:16:26 -0500

    practice 1 and 2

---
+++ practice_one/app.js
@@ -0,0 +1,156 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+const cardArray = [
+    {
+        name: 'a320',
+        img: 'images/320.jpg'
+    },
+    {
+        name: 'a350',
+        img: 'images/350a.jpg'
+    },
+    {
+        name: 'a380',
+        img: 'images/380.jpg'
+    },
+    {
+        name: '380',
+        img: 'images/380e.jpg'
+    },
+    {
+        name: 'one',
+        img: 'images/one.jpg'
+    }/*,
+    {
+        name: 'two',
+        img: 'images/two.jpg'
+    },
+    {
+        name: 'three',
+        img: 'images/three.jpg'
+    }*/
+    ,
+    {
+        name: 'lobo',
+        img: 'images/lobo.jpeg'
+    },
+    {
+        name: 'sierra',
+        img: 'images/sierra.jpeg'
+    }
+    ,
+    {
+        name: 'palmeras',
+        img: 'images/palmeras.jpeg'
+    },{
+        name: 'a320',
+        img: 'images/320.jpg'
+    },
+    {
+        name: 'a350',
+        img: 'images/350a.jpg'
+    },
+    {
+        name: 'a380',
+        img: 'images/380.jpg'
+    },
+    {
+        name: '380',
+        img: 'images/380e.jpg'
+    },
+    {
+        name: 'one',
+        img: 'images/one.jpg'
+    }/*,
+    {
+        name: 'two',
+        img: 'images/two.jpg'
+    },
+    {
+        name: 'three',
+        img: 'images/three.jpg'
+    }*/
+    ,
+    {
+        name: 'palmeras',
+        img: 'images/palmeras.jpeg'
+    },
+    {
+        name: 'lobo',
+        img: 'images/lobo.jpeg'
+    },
+    {
+        name: 'sierra',
+        img: 'images/sierra.jpeg'
+    }
+
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const grid = document.querySelector('.grid');
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
+
+//check for matches
+ function checkForMatch() {
+     var cards = document.querySelectorAll('img')
+     const optionOneId = cardsChosenId[0]
+     const optionTwoId = cardsChosenId[1]
+     if( cardsChosen[0] === cardsChosen[1]){
+         cards[optionOneId].setAttribute('src', 'images/white.png')
+         cards[optionTwoId].setAttribute('src', 'images/white.png')
+         cards[optionOneId].style.pointerEvents = "none";
+         cards[optionTwoId].style.pointerEvents = "none";
+         cardsWon.push(cardsChosen)   
+         //document.querySelector('#exampleModalCenter').modal();
+         $('#exampleModalCenter').modal()
+    }else{
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+         cards[optionTwoId].setAttribute('src', 'images/blank.png')
+         //alert('sorry try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if(cardsWon.length === cardArray.length/2){
+        resultDisplay.textContent = 'Congratulations! You found them all'
+        $('#championModalCenter').modal()
+    }
+ }
+
+function createBoard(){
+    for(let i=0; i< cardArray.length; i++){
+        var card = document.createElement('img')
+        card.classList.add('img-fluid')
+        card.classList.add('img-thumbnail')
+        card.classList.add('w-25')
+        card.classList.add('p-1')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card); 
+    }
+}
+
+function flipCard(e){
+    //console.log('e: ', e)
+    var cardId = this.getAttribute('data-id')
+    console.log('cardId: ', cardId)
+    console.log('name: ', cardArray[cardId].name)
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2 ){
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createBoard()
+
+
+
+
+});
---
+++ practice_two/app.js
@@ -0,0 +1,51 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare(){
+    square.forEach(className =>{
+        className.classList.remove('mole')
+    })
+
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    //console.log(randomPosition)
+    randomPosition.classList.add('mole')
+
+    //assing the id of the randomPosition to hitPosition for us to use later
+    hitPosition = randomPosition.id
+}
+
+square.forEach(id =>{
+    //console.log('ID: ', id)
+    id.addEventListener('mouseup', ()=>{
+        if(id.id === hitPosition ){
+            result = result +1
+            score.textContent = result
+        }
+    })
+})
+
+function moveMole(){
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+    console.log('timerId: ', timerId)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime --
+    timeLeft.textContent = currentTime 
+
+    if(currentTime === 0){
+        clearInterval(timerId)
+        document.querySelector('#resultScore').textContent = result
+        $('#modalResultCenter').modal()
+    }
+} 
+
+let timerId = setInterval(countDown, 1000)
